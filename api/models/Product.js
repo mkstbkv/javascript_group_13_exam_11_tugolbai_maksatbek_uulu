049eef63019ae33c3a768ff4b3cf6025e18909e3
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-03-12 12:48:42 +0600

    сущности юзера, товара и категории

---
+++ api/models/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ProductSchema = new Schema({
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
